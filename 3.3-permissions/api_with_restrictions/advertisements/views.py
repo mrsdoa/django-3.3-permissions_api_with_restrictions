3c818578a00d19087517b71a491ae57c4255aa5a
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-09-03 22:49:09 +0300

    Add files via upload

---
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from .permissions import IsOwner
+from .filters import AdvertisementFilter
+from django_filters import rest_framework as filters
+from .serializers import AdvertisementSerializer
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action == "create":
+            return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwner()]
+        return []
