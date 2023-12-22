5af636d378a41dded2281675b296bae0f35597eb
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-12-23 00:46:39 +0300

    Update on Throttling settings.py

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -76,7 +76,17 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+        'user': '100/day',
+        'robots': '3/day'
+    }
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
